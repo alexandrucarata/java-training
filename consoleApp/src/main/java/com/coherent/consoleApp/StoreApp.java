f8eb94dee22f2caa349ad88debe0a228d04db904
alexandrucarata <alexandrucarata@gmail.com>
2023-03-07 11:38:59 +0200

    implemented chain of responsibility pattern

--- consoleApp/src/main/java/com/coherent/consoleApp/StoreApp.java
+++ consoleApp/src/main/java/com/coherent/consoleApp/StoreApp.java
@@ -1,8 +1,8 @@
 package com.coherent.consoleApp;
 
+import com.coherent.consoleApp.interaction.*;
 import com.coherent.store.Store;
 import com.coherent.store.helpers.RandomStorePopulator;
-import com.coherent.store.xmlsort.ComparatorClass;
 
 import java.util.Scanner;
 
@@ -11,23 +11,16 @@ public class StoreApp {
         Store store = Store.getInstance();
         RandomStorePopulator storePopulator = new RandomStorePopulator(store);
         Scanner scanner = new Scanner(System.in);
-        ComparatorClass comparator = new ComparatorClass(store);
         storePopulator.addProductsToCategories();
         store.printAllStoreProducts();
 
         while (true) {
             System.out.println("\nChoose an action:\n\t-> sort\n\t-> top\n\t-> quit");
             System.out.print("Action -> ");
-            String action = scanner.next();
-            if (action.equalsIgnoreCase("quit")) {
-                break;
-            } else if (action.equalsIgnoreCase("sort")) {
-                comparator.getSortedProducts();
-            } else if (action.equalsIgnoreCase("top")) {
-                comparator.getTop5Products();
-            } else {
-                System.out.println("Warning: Invalid Action. Try again...");
-            }
+            Action action = new Action(scanner.next(), store);
+            QuitHandler actionChain = new QuitHandler();
+            actionChain.link(new SortingHandler()).link(new Top5Handler());
+            actionChain.perform(action);
         }
     }
 }
